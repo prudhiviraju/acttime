fe68260cb22de5dd60a82c99966b6dea74417bb0
prudhvi R@zz <prudhiviraju73@gmail.com>
2019-11-03 23:04:07 +0530

    latest updated

--- src/test/java/com/acttime/qa/testcases/Loginpagetest.java
+++ src/test/java/com/acttime/qa/testcases/Loginpagetest.java
@@ -1,17 +1,28 @@
 package com.acttime.qa.testcases;
 
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.acttime.qa.base.Baseclass;
 import com.acttime.qa.pages.Homepage;
 import com.acttime.qa.pages.Loginpage;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.relevantcodes.extentreports.ExtentReports;
 
 public class Loginpagetest extends Baseclass{
 
 	Loginpage loginpage;
 	Homepage homepage;
+	
+	
+	
+	private static Logger logger = LogManager.getLogger(Loginpagetest.class);
+	
 	
 	
 	public Loginpagetest() throws Exception {
@@ -20,7 +31,17 @@ public class Loginpagetest extends Baseclass{
 	
 	@Test
 	public void setup() {
+
 		
+		ExtentHtmlReporter htmlreport = new ExtentHtmlReporter("./report/newhtmlreport.html");
+		com.aventstack.extentreports.ExtentReports extent = new com.aventstack.extentreports.ExtentReports();
+		
+		extent.attachReporter(htmlreport);
+		ExtentTest logger =extent.createTest("logintest");
+		
+		
+		
+		logger.log(Status.INFO, "login is done");
 	    try {
 	    	intialize("chrome");
 			Loginpage log = new Loginpage();
@@ -28,12 +49,16 @@ public class Loginpagetest extends Baseclass{
 			String expected =log.verifypagetitle();
 			Assert.assertEquals(expected, "actiTIME - Login");
 		    log.login(pro.getProperty("Username"),pro.getProperty("Password"));
+	
+		    logger.log(Status.INFO,"log in done" );
+		    
 		    
 			Thread.sleep(7000);
 		} catch (InterruptedException e) {
 
 			System.out.println("the exception is" + e.getMessage());
 		}
+	    extent.flush();
 		driver.quit();
 		
 		
